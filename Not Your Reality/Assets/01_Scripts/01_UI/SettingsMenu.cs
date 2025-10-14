caaa4f9001aa77f9df74ccf0f0553ac1cf8fc558
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-10-14 16:12:06 +0200

    [*] Simple Settings
    Added Resolution Settings
    Added Fullscreen Toggle

---
+++ Not Your Reality/Assets/01_Scripts/01_UI/SettingsMenu.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SettingsMenu : MonoBehaviour
+{
+    private Resolution[] _resolutions;
+    private List<string> _resOptions;
+    private string _resOption;
+    private int _resIndex;
+    [SerializeField] private TMP_Dropdown resDropDown;
+    
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 1;
+        _resOptions = new List<string>();
+        _resolutions = Screen.resolutions;
+        resDropDown.ClearOptions();
+
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            _resOption = $"{_resolutions[i].width} x {_resolutions[i].height}";
+            _resOptions.Add(_resOption);
+            if (_resolutions[i].width == Screen.currentResolution.width &&
+                _resolutions[i].height == Screen.currentResolution.height)
+            {
+                _resIndex = i;
+            }
+        }
+        resDropDown.AddOptions(_resOptions);
+        resDropDown.value = _resIndex;
+        resDropDown.RefreshShownValue();
+    }
+
+    public void SetResolution(int resIndex)
+    {
+        var resolution = _resolutions[_resIndex];
+        Screen.SetResolution(resolution.width, resolution.height, fullscreenMode: FullScreenMode.FullScreenWindow);
+    }
+
+    public void SetFullscreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
