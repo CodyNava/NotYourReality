a36c8cb314502b411527d4e9a6dc751db8acd78f
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-10-17 10:47:23 +0200

    [*] Removed some Jittering
    Less Jitter when Holding an item without moving

--- Not Your Reality/Assets/01_Scripts/02_Interactions/02_Interaction_System/02_Interactions/Move_Object.cs
+++ Not Your Reality/Assets/01_Scripts/02_Interactions/02_Interaction_System/02_Interactions/Move_Object.cs
@@ -9,24 +9,32 @@ public class Move_Object : Interactable_Base
     public float Weight => weight;
 
     private Rigidbody _rb;
+    private bool _isHeld;
+    private Camera _cam;
 
     private void Awake()
     {
+        _cam = FindFirstObjectByType<Camera>();
         _rb = GetComponent<Rigidbody>();
     }
 
     public override void OnInteract()
     {
         base.OnInteract();
+        _isHeld = true;
         _rb.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if (!_isHeld) return;
         MoveItem();
     }
 
     private void MoveItem()
     {
-        var cam = FindFirstObjectByType<Camera>();
         var currentPosition = gameObject.transform.position;
-        var targetPosition = cam.transform.position + cam.transform.forward * holdingDistance;
+        var targetPosition = _cam.transform.position + _cam.transform.forward * holdingDistance;
         var time = Time.deltaTime * dragSpeed;
         var lerp = Vector3.Lerp(currentPosition, targetPosition, time);
         _rb.MovePosition(lerp);
@@ -34,6 +42,7 @@ public class Move_Object : Interactable_Base
 
     public void Release()
     {
+        _isHeld = false;
         _rb.useGravity = true;
     }
 }
