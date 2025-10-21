9f702749450c1d0a6b92211353286fb1a28eb749
MikoHeine <Miko.Heine@games-academy.de>
2025-10-21 19:24:54 +0200

    [*] Changed Puzzle so it Works on new Doors

--- Not Your Reality/Assets/01_Scripts/02_Puzzles/Floorplate_Weight_Trigger.cs
+++ Not Your Reality/Assets/01_Scripts/02_Puzzles/Floorplate_Weight_Trigger.cs
@@ -3,76 +3,80 @@ using UnityEngine;
 
 public class Floorplate_Weight_Trigger : MonoBehaviour
 {
-    [Space]
-    [Tooltip("The weight that needs to be reached for the plate to trigger")]
-    [SerializeField] private float goalWeight;
+   [Space]
+   [Tooltip("The weight that needs to be reached for the plate to trigger")]
+   [SerializeField] private float goalWeight;
 
-    [Tooltip("The distance the platform will move once it is activated")]
-    [SerializeField] private float platformMoveDistance;
+   [Tooltip("The distance the platform will move once it is activated")]
+   [SerializeField] private float platformMoveDistance;
 
-    [Tooltip("The speed the platform will move at once it is activated")]
-    [SerializeField] private float platformMoveSpeed;
-    [Tooltip("The target effected by the plate (e.g. a Door)")]
-    [SerializeField] private GameObject winTarget;
-    
-    private List<Move_Object> _objects;
-    private Rigidbody _rb;
-    private bool _hasWon;
+   [Tooltip("The speed the platform will move at once it is activated")]
+   [SerializeField] private float platformMoveSpeed;
+   [Tooltip("The target effected by the plate (e.g. a Door)")]
+   [SerializeField] private GameObject winTarget;
+   [SerializeField] private bool toggleDoor;
+   [SerializeField] private bool toggleObject;
 
-    private Vector3 _startPos;
-    private Vector3 _targetPos;
+   private List<Move_Object> _objects;
+   private Rigidbody _rb;
+   private bool _hasWon;
+   private Collider _collider;
 
-    private void Awake()
-    {
-        _objects = new List<Move_Object>();
-        _objects.Clear();
-        _rb = GetComponent<Rigidbody>();
-        _startPos = transform.position;
-        _targetPos = _startPos;
-    }
+   private Vector3 _startPos;
+   private Vector3 _targetPos;
 
-    private void FixedUpdate()
-    {
-        var moveTo = Vector3.Lerp(transform.position, _targetPos, Time.fixedDeltaTime * platformMoveSpeed);
-        _rb.MovePosition(moveTo);
-    }
+   private void Awake()
+   {
+      _objects = new List<Move_Object>();
+      _objects.Clear();
+      _rb = GetComponent<Rigidbody>();
+      _startPos = transform.position;
+      _targetPos = _startPos;
+      if (toggleDoor) _collider = winTarget.GetComponent<Collider>();
+   }
 
-    private void CheckWin()
-    {
-        var currentWeight = 0f;
-        foreach (var moveObject in _objects)
-        {
-            currentWeight += moveObject.Weight;
-        }
-        if (currentWeight >= goalWeight && !_hasWon)
-        {
-            _targetPos = _startPos + Vector3.down * platformMoveDistance;
-            _hasWon = true;
-            winTarget.SetActive(false);
-            Debug.Log("You did it");
-        }
-        else if (currentWeight < goalWeight && _hasWon)
-        {
-            _hasWon = false;
-            _targetPos = _startPos;
-            winTarget.SetActive(true);
-            Debug.Log("Not enough");
-        }
-    }
+   private void FixedUpdate()
+   {
+      var moveTo = Vector3.Lerp(transform.position, _targetPos, Time.fixedDeltaTime * platformMoveSpeed);
+      _rb.MovePosition(moveTo);
+   }
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (!other.CompareTag("Weight")) return;
-        var moveObject = other.GetComponent<Move_Object>();
-        _objects.Add(moveObject);
-        CheckWin();
-    }
+   private void CheckWin()
+   {
+      var currentWeight = 0f;
+      foreach (var moveObject in _objects) { currentWeight += moveObject.Weight; }
 
-    private void OnTriggerExit(Collider other)
-    {
-        if (!other.CompareTag("Weight")) return;
-        var moveObject = other.GetComponent<Move_Object>();
-        _objects.Remove(moveObject);
-        CheckWin();
-    }
+      if (currentWeight >= goalWeight && !_hasWon)
+      {
+         _targetPos = _startPos + Vector3.down * platformMoveDistance;
+         _hasWon = true;
+         Debug.Log("You did it");
+         if (toggleObject) winTarget.SetActive(false);
+         if (toggleDoor) _collider.enabled = true;
+      }
+      else if (currentWeight < goalWeight && _hasWon)
+      {
+         _hasWon = false;
+         _targetPos = _startPos;
+         Debug.Log("Not enough");
+         if (toggleObject) winTarget.SetActive(true);
+         if (toggleDoor) _collider.enabled = false;
+      }
+   }
+
+   private void OnTriggerEnter(Collider other)
+   {
+      if (!other.CompareTag("Weight")) return;
+      var moveObject = other.GetComponent<Move_Object>();
+      _objects.Add(moveObject);
+      CheckWin();
+   }
+
+   private void OnTriggerExit(Collider other)
+   {
+      if (!other.CompareTag("Weight")) return;
+      var moveObject = other.GetComponent<Move_Object>();
+      _objects.Remove(moveObject);
+      CheckWin();
+   }
 }
--- Not Your Reality/Assets/01_Scripts/02_Puzzles/Switch_Sequence.cs
+++ Not Your Reality/Assets/01_Scripts/02_Puzzles/Switch_Sequence.cs
@@ -4,58 +4,59 @@ using UnityEngine;
 
 public class Switch_Sequence : MonoBehaviour
 {
-    [Tooltip("This is the order the switches are supposed to be pressed in")]
-    [SerializeField] private List<Sequence_Switch> sequenceOrder;
-    private List<Sequence_Switch> _playerOrder;
-    private bool _hasWon;
+   [Tooltip("This is the order the switches are supposed to be pressed in")]
+   [SerializeField] private List<Sequence_Switch> sequenceOrder;
+   [SerializeField] private bool toggleDoor;
+   [SerializeField] private bool toggleObject;
+   private List<Sequence_Switch> _playerOrder;
+   private bool _hasWon;
+   private Collider _collider;
 
-    private void Awake()
-    {
-        _playerOrder = new List<Sequence_Switch>();
-    }
+   private void Awake()
+   {
+      _playerOrder = new List<Sequence_Switch>();
+      if (!toggleDoor) return;
+      _collider = GetComponent<Collider>();
+      _collider.enabled = false;
+   }
 
-    public void AddSwitch(Sequence_Switch sequenceSwitch)
-    {
-        _playerOrder.Add(sequenceSwitch);
-        if (_playerOrder.Count == sequenceOrder.Count && !_hasWon)
-        {
-            StartCoroutine(WinCondition());
-        }
-    }
+   public void AddSwitch(Sequence_Switch sequenceSwitch)
+   {
+      _playerOrder.Add(sequenceSwitch);
+      if (_playerOrder.Count == sequenceOrder.Count && !_hasWon) { StartCoroutine(WinCondition()); }
+   }
 
-    private IEnumerator WinCondition()
-    {
-        yield return new WaitForSeconds(0.2f);
-        var counter = 0;
-        for (var i = 0; i < _playerOrder.Count; i++)
-        {
-            if (_playerOrder[i] == sequenceOrder[i])
-            {
-                counter++;
-                continue;
-            }
-            break;
-        }
-        if (counter == sequenceOrder.Count)
-        {
-            Debug.Log("You Win");
-            _hasWon = true;
-            gameObject.SetActive(false);
-        }
-        else
-        {
-            Reset();
-        }
-    }
+   private IEnumerator WinCondition()
+   {
+      yield return new WaitForSeconds(0.2f);
+      var counter = 0;
+      for (var i = 0; i < _playerOrder.Count; i++)
+      {
+         if (_playerOrder[i] == sequenceOrder[i])
+         {
+            counter++;
+            continue;
+         }
 
-    private void Reset()
-    {
-        foreach (var seqSwitch in _playerOrder)
-        {
-            seqSwitch.ToggleOff();
-        }
+         break;
+      }
 
-        Debug.Log("Try Again");
-        _playerOrder.Clear();
-    }
+      if (counter == sequenceOrder.Count)
+      {
+         Debug.Log("You Win");
+         _hasWon = true;
+         if (toggleObject) { gameObject.SetActive(false); }
+
+         if (toggleDoor) { _collider.enabled = true; }
+      }
+      else { Reset(); }
+   }
+
+   private void Reset()
+   {
+      foreach (var seqSwitch in _playerOrder) { seqSwitch.ToggleOff(); }
+
+      Debug.Log("Try Again");
+      _playerOrder.Clear();
+   }
 }
