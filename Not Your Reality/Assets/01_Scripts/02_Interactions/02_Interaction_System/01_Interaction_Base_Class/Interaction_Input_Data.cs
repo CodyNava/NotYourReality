f70739725b9126c23fa5196f5a3e5402c2dde0e8
Jan-Lukas Dehne <kingdomeheart16@gmail.com>
2025-10-23 16:58:54 +0200

    [*] Reworked Interaction System with new Input

--- Not Your Reality/Assets/01_Scripts/02_Interactions/02_Interaction_System/01_Interaction_Base_Class/Interaction_Input_Data.cs
+++ Not Your Reality/Assets/01_Scripts/02_Interactions/02_Interaction_System/01_Interaction_Base_Class/Interaction_Input_Data.cs
@@ -1,35 +1,18 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "InteractionInputData", menuName = "InteractionSystem/InputData")]
-public class Interaction_Input_Data : ScriptableObject
+public class InteractionInputData : ScriptableObject
 {
-    
-    private bool _interactedClicked;
-    private bool _interactedHold;
-    private bool _interactedReleased;
+    public bool InteractedClicked { get; set; }
 
-    public bool InteractedClicked
-    {
-        get => _interactedClicked;
-        set => _interactedClicked = value;
-    }
+    public bool InteractedHold { get; set; }
 
-    public bool InteractedHold
-    {
-        get => _interactedHold;
-        set => _interactedHold = value;
-    }
-
-    public bool InteractedReleased
-    {
-        get => _interactedReleased;
-        set => _interactedReleased = value;
-    }
+    public bool InteractedReleased { get; set; }
 
     public void Reset()
     {
-        _interactedClicked = false;
-        _interactedReleased = false;
-        _interactedHold = false;
+        InteractedClicked = false;
+        InteractedReleased = false;
+        InteractedHold = false;
     }
 }
